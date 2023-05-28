661d0d548b5c4b33c5488f7113d2a20e8169e8b0
charlesmatte <matte.charles@gmail.com>
2023-05-27 21:19:49 -0400

    End of section 9

--- Errors/ApiException.cs
+++ Errors/ApiException.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace API.Errors
 {
     // A class called ApiException that does not inherit, has a constructor and the three following properties: StatusCode, Message and Details
